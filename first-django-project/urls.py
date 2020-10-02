154645dbe67224358799daf315bfbe76fa6c5151
Ling Xu <36904516+xulingggg@users.noreply.github.com>
2020-10-01 20:16:33 -0700

    Add files via upload

---
+++ first-django-project/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path     
+from . import views # the . indicates that the views file can be found in the same directory as this file
+urlpatterns = [
+    path('', views.index),   
+    path('blogs', views.index),
+    path('redirects', views.root),
+    path('blogs/new', views.new),
+    path('blogs/create', views.create),
+    path('blogs/<int:number>', views.show),
+    path('blogs/<int:number>/edit', views.edit),
+    path('blogs/<int:number>/delete', views.destroy),
+    #path('blogs/json', views.json),
+]
---
+++ first-django-project/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, HttpResponse, redirect ## add redirect to import statement
+
+def index(request):
+    return HttpResponse("placeholder to later display a list of all blogs")
+
+def root(request):
+    return redirect("/blogs")
+
+def new (request):
+    return HttpResponse('placeholder to display a new form to create a new blog')
+
+def create(request):
+    return redirect('/')
+
+def show(request, number=66):
+    return HttpResponse(f'placeholder to display blog number: {number}')
+
+def edit(request, number=66):
+    return HttpResponse(f'placeholder to edit blog {number}')
+
+def destroy(request, number = 66):
+    return redirect('/blogs')
+
+def json(request):
+    pass
